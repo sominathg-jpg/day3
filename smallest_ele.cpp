e3ca353b7192cbe1c2e65fa7380db3b0a3a641d4
sominathg-jpg <sominathg@sigasystems.com>
2025-08-22 10:45:28 +0530

    sum of all elements of the array

--- smallest_ele.cpp
+++ smallest_ele.cpp
@@ -3,18 +3,28 @@ using namespace ::std;
 int main()
 {
 
-    int a[] = {45, 23, 99, 67, 12, 34, 88, 76, 51, 10,0};
+    int a[] = {45, 23, 99, 67, 12, 34, 88, 76, 51, 10, 0};
 
-    int smallest = a[0];
+    // int smallest = a[0];
 
-    for (int i = 0; i < (sizeof(a) / sizeof(a[0])) ; i++)
+    // for (int i = 0; i < (sizeof(a) / sizeof(a[0])) ; i++)
+    // {
+    //     if (a[i] < smallest)
+    //     {
+    //         smallest = a[i];
+    //     }
+    // }
+
+    // cout << smallest << endl;
+
+    // summ of all element of the array
+
+    int sum = 0;
+    for (int i = 0; i < (sizeof(a) / sizeof(a[0])); i++)
     {
-        if (a[i] < smallest)
-        {
-            smallest = a[i];
-        }
-    }
 
-    cout << smallest << endl;
+        sum += a[i];
+    }
+    cout << sum << endl;
     return 0;
 }
