d2069042a116db2563e8b64b854b1524cbc61541
sominathg-jpg <sominathg@sigasystems.com>
2025-08-22 10:34:19 +0530

    array basics

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
-// array and linked list priparation 
+// array and linked list priparation
 
+#include <iostream>
+using namespace ::std;
+int main()
+{
 
-#include<iostream>
-int main(){
+    // declaration of array
+    // initialization of the array
+    int a[] = {1, 2, 3, 4, 5};
+    // accessing and prinnting element from the array
 
+    for (int i = 0; i < (sizeof(a) / sizeof(a[0])) - 1; i++)
+    {
 
-
-
-    
+        cout << a[i] << endl;
+    }
 
     return 0;
 }
